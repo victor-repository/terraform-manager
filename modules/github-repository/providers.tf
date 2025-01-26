2ca5a898dcbc154722e05d83e81d19446c247d26
Victor Hernandez <victor.mx@gmail.com>
2025-01-25 23:16:50 -0600

    Update modules.

--- modules/github-repository/providers.tf
+++ modules/github-repository/providers.tf
@@ -12,6 +12,7 @@ provider "github" {
   token = "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" #var.token # or `GITHUB_TOKEN`
 }
 
+
 # Add a user to the organization
 # resource "github_membership" "membership_for_user_x" {
 #   # ...
